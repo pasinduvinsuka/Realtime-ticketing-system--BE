36e7149001b2a01883ef286f6e20ed802a332c91
Inivinska] <nimsara.pasindu@inivosglobal.com>
2024-12-09 08:24:55 +0530

    initial commit

---
+++ src/test/java/com/example/ticketingSystem/TicketingSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.ticketingSystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TicketingSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
